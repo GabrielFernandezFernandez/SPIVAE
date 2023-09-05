c2652d63552c125f94e1f352207b4d1fc850f90f
Gabriel Fernández Fernández <gabriel.fernandez@icfo.eu>
2023-09-05 16:16:05 +0200

    Clean source nbs

---
+++ SPIVAE/imports.py
@@ -0,0 +1,7 @@
+# convenient imports
+import matplotlib.pyplot as plt, numpy as np, andi_datasets
+from tqdm.auto import tqdm, trange
+from fastai.vision.all import *
+from .data import *
+from .models import *
+from .utils import *
